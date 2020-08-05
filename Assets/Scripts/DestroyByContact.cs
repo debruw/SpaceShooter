c79aa782bbfbe5356a4dde3d99b251fdbb03b548
Ebru Çakar <ebru_cakar_11@hotmail.com>
2020-08-05 20:13:24 +0300

    Initial commit

---
+++ Assets/Scripts/DestroyByContact.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyByContact : MonoBehaviour
+{
+    public GameObject explosionEffect, playerExplosionEffect;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Boundry"))
+        {
+            return;
+        }
+        if (other.CompareTag("Player"))
+        {
+            Instantiate(playerExplosionEffect, transform.position, Quaternion.identity);
+        }
+        else
+        {
+            Instantiate(explosionEffect, transform.position, Quaternion.identity);
+        }        
+        Destroy(other.gameObject);
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/RandomRotater.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomRotater : MonoBehaviour
+{
+    public float tumble;
+    Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.angularVelocity = Random.insideUnitSphere * tumble;
+    }
+}
